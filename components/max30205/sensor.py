8f96480eb83b80af52f9c712e2933fe707f0b5b2
CmPi <CmPi@users.noreply.github.com>
2025-12-09 15:33:16 +0100

    New test

--- components/max30205/sensor.py
+++ components/max30205/sensor.py
@@ -1,6 +1,5 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import automation
 from esphome.components import sensor
 from esphome.const import (
     CONF_ID,
@@ -8,8 +7,10 @@ from esphome.const import (
     ICON_THERMOMETER,
 )
 
+CONF_ADDRESS = "address"
+
 max30205_ns = cg.esphome_ns.namespace("max30205")
-MAX30205Component = max30205_ns.class_("MAX30205Component", cg.PollingComponent, sensor.Sensor)
+MAX30205Component = max30205_ns.class_("MAX30205Component", cg.PollingComponent)
 
 CONFIG_SCHEMA = sensor.sensor_schema(
     MAX30205Component,
@@ -19,14 +20,13 @@ CONFIG_SCHEMA = sensor.sensor_schema(
 ).extend(
     {
         cv.GenerateID(): cv.declare_id(MAX30205Component),
-        cv.Optional("address", default=0x48): cv.i2c_address,
+        cv.Optional(CONF_ADDRESS, default=0x48): cv.i2c_address,
     }
 ).extend(cv.polling_component_schema("10s"))
-
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await sensor.register_sensor(var, config)
-
-    cg.add(var.set_address(config["address"]))
+    sens = await sensor.new_sensor(config)
+    cg.add(var.set_sensor(sens))
+    cg.add(var.set_address(config[CONF_ADDRESS]))
