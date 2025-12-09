04f6e10dd36e40b27c53ce4b33fd9fa1fb429f41
CmPi <CmPi@users.noreply.github.com>
2025-12-09 16:10:43 +0100

    simplified

--- components/max30205/max30205.h
+++ components/max30205/max30205.h
@@ -1,69 +1,76 @@
 #pragma once
 
 #include "esphome.h"
-#include "esphome/components/i2c/i2c.h"
-#include "esphome/components/sensor/sensor.h"
 
-#define MAX30205_ADDRESS 0x48
-#define MAX30205_TEMPERATURE 0x00
+#ifdef ESP32
+  #include <Wire.h>
+#elif defined(ESP8266)
+  #include <Wire.h>
+#endif
+
+namespace esphome {
+namespace max30205 {
+
+#define MAX30205_I2C_ADDRESS 0x48
 #define MAX30205_CONFIGURATION 0x01
-#define MAX30205_THYST 0x02
-#define MAX30205_TOS 0x03
+#define MAX30205_TEMPERATURE_REGISTER 0x00
 
-class MAX30205Component : public PollingComponent, public esphome::sensor::Sensor, public i2c::I2CDevice {
-public:
-    float fLastTemperature = 0;
-    uint8_t sensorAddress = MAX30205_ADDRESS;
+class MAX30205Component : public PollingComponent, public sensor::Sensor {
+ public:
+  MAX30205Component(uint32_t update_interval = 10000)
+      : PollingComponent(update_interval), fLastTemperature(NAN) {}
 
-    MAX30205Component() : PollingComponent(10000) {}
+  void setup() override {
+    Wire.begin();
+    begin();
+  }
 
-    void set_address(uint8_t address) { sensorAddress = address; }
+  void dump_config() override {
+    ESP_LOGCONFIG("MAX30205", "MAX30205 sensor:");
+    LOG_UPDATE_INTERVAL(this);
+  }
 
-    void setup() override { begin(); }
-
-    void dump_config() override {
-        LOG_SENSOR("", "MAX30205", this);
-        LOG_I2C_DEVICE(this);
-        LOG_UPDATE_INTERVAL(this);
+  void update() override {
+    float fCurrentTemperature = getTemperature();
+    if (isnan(fLastTemperature) || fCurrentTemperature != fLastTemperature) {
+      publish_state(fCurrentTemperature);
+      fLastTemperature = fCurrentTemperature;
     }
+  }
 
-    void update() override {
-        float fCurrentTemperature = getTemperature();
-        if (fCurrentTemperature != fLastTemperature) {
-            publish_state(fCurrentTemperature);
-            fLastTemperature = fCurrentTemperature;
-        }
+  float get_setup_priority() const override { return setup_priority::BUS; }
+
+ protected:
+  float fLastTemperature;
+
+  void begin() {
+    writeRegister(MAX30205_CONFIGURATION, 0x00);
+  }
+
+  void writeRegister(uint8_t reg, uint8_t value) {
+    Wire.beginTransmission(MAX30205_I2C_ADDRESS);
+    Wire.write(reg);
+    Wire.write(value);
+    Wire.endTransmission();
+  }
+
+  float getTemperature() {
+    uint8_t data[2];
+    readRegisters(MAX30205_TEMPERATURE_REGISTER, data, 2);
+    int16_t raw = (data[0] << 8) | data[1];
+    return raw * 0.00390625f; // Conversion selon la datasheet MAX30205
+  }
+
+  void readRegisters(uint8_t reg, uint8_t *buffer, uint8_t length) {
+    Wire.beginTransmission(MAX30205_I2C_ADDRESS);
+    Wire.write(reg);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MAX30205_I2C_ADDRESS, length);
+    for (uint8_t i = 0; i < length; i++) {
+      buffer[i] = Wire.read();
     }
-
-    float get_setup_priority() const override { return esphome::setup_priority::BUS; }
-
-protected:
-    float getTemperature() {
-        uint8_t readRaw[2] = {0};
-        I2CreadBytes(sensorAddress, MAX30205_TEMPERATURE, readRaw, 2);
-        int16_t raw = (readRaw[0] << 8) | readRaw[1];
-        return raw * 0.00390625;
-    }
-
-    void begin() {
-        I2CwriteByte(sensorAddress, MAX30205_CONFIGURATION, 0x00);
-        I2CwriteByte(sensorAddress, MAX30205_THYST, 0x00);
-        I2CwriteByte(sensorAddress, MAX30205_TOS, 0x00);
-    }
-
-    void I2CwriteByte(uint8_t address, uint8_t subAddress, uint8_t data){
-        Wire.beginTransmission(address);
-        Wire.write(subAddress);
-        Wire.write(data);
-        Wire.endTransmission();
-    }
-
-    void I2CreadBytes(uint8_t address, uint8_t subAddress, uint8_t *dest, uint8_t count){
-        Wire.beginTransmission(address);
-        Wire.write(subAddress);
-        Wire.endTransmission(false);
-        Wire.requestFrom(address, count);
-        for (uint8_t i = 0; i < count; i++)
-            dest[i] = Wire.read();
-    }
+  }
 };
+
+}  // namespace max30205
+}  // namespace esphome
