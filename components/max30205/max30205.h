a9c31e95ffbd149bf1c6abeb5d91cfcd0f4f4e35
CmPi <CmPi@users.noreply.github.com>
2025-12-09 15:55:45 +0100

    updated again

--- components/max30205/max30205.h
+++ components/max30205/max30205.h
@@ -4,30 +4,22 @@
 #include "esphome/components/i2c/i2c.h"
 #include "esphome/components/sensor/sensor.h"
 
-// Adresse I2C par défaut
-#define MAX30205_ADDRESS        0x48
-
-// Registres
-#define MAX30205_TEMPERATURE    0x00
-#define MAX30205_CONFIGURATION  0x01
-#define MAX30205_THYST          0x02
-#define MAX30205_TOS            0x03
+#define MAX30205_ADDRESS 0x48
+#define MAX30205_TEMPERATURE 0x00
+#define MAX30205_CONFIGURATION 0x01
+#define MAX30205_THYST 0x02
+#define MAX30205_TOS 0x03
 
 class MAX30205Component : public PollingComponent, public esphome::sensor::Sensor, public i2c::I2CDevice {
 public:
     float fLastTemperature = 0;
     uint8_t sensorAddress = MAX30205_ADDRESS;
 
-    // Constructeur avec intervalle de polling par défaut
     MAX30205Component() : PollingComponent(10000) {}
 
-    void set_address(uint8_t address) { 
-        sensorAddress = address; 
-    }
+    void set_address(uint8_t address) { sensorAddress = address; }
 
-    void setup() override {
-        begin();
-    }
+    void setup() override { begin(); }
 
     void dump_config() override {
         LOG_SENSOR("", "MAX30205", this);
@@ -43,9 +35,7 @@ public:
         }
     }
 
-    float get_setup_priority() const override { 
-        return esphome::setup_priority::BUS; 
-    }
+    float get_setup_priority() const override { return esphome::setup_priority::BUS; }
 
 protected:
     float getTemperature() {
@@ -66,14 +56,6 @@ protected:
         Wire.write(subAddress);
         Wire.write(data);
         Wire.endTransmission();
-    }
-
-    uint8_t I2CreadByte(uint8_t address, uint8_t subAddress){
-        Wire.beginTransmission(address);
-        Wire.write(subAddress);
-        Wire.endTransmission(false);
-        Wire.requestFrom(address, (uint8_t) 1);
-        return Wire.read();
     }
 
     void I2CreadBytes(uint8_t address, uint8_t subAddress, uint8_t *dest, uint8_t count){
